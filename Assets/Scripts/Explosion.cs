c3b8d780962f8466094abf5f8dd349630bdf2ad3
Jared Halpern <jared@jaredhalpern.com>
2019-03-31 19:22:11 -0400

    finished project

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour, IExecutableAction
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void ExecuteAction()
+    {
+        animator.SetTrigger("explode");
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IExecutableAction
+{
+    void ExecuteAction();
+}
+
+public class Timer : MonoBehaviour
+{
+    public float waitTime = 3f;
+    MonoBehaviour[] monoBehaviours;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monoBehaviours = gameObject.GetComponents<MonoBehaviour>();
+        StartCoroutine(TimerCoroutine());
+    }
+
+    IEnumerator TimerCoroutine()
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime <= waitTime)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        CheckExecutableActions();
+    }
+
+    private void CheckExecutableActions()
+    {
+        foreach (MonoBehaviour monoBehaviour in monoBehaviours)
+        {
+            if (monoBehaviour is IExecutableAction)
+            {
+                IExecutableAction actionableObject = (IExecutableAction)monoBehaviour;
+                actionableObject.ExecuteAction();
+            }
+        }
+    }
+}
